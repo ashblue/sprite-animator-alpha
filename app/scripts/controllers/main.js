1d2988ea93f81912a5f8f7c0262c44da80a75e7b
ashblue <ash@blueashes.com>
2014-06-06 16:47:43 -0700

    Partially complete timeline GUI mockup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,4 +19,31 @@
             controllerAs: 'header'
         };
     });
+
+    app.directive('animationManager', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'views/animation-manager.html'
+//            controller: 'SpriteHeader',
+//            controllerAs: 'header'
+        };
+    });
+
+    app.directive('animationTimeline', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'views/animation-timeline.html'
+//            controller: 'SpriteHeader',
+//            controllerAs: 'header'
+        };
+    });
+
+    app.filter('range', function() {
+        return function(input, total) {
+            total = parseInt(total);
+            for (var i=0; i<total; i++)
+                input.push(i);
+            return input;
+        };
+    });
 })();
